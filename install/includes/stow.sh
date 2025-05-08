de82661bc7072a13be7a7a54cab962049a9e6c0f
McZlik <github@mczlik.com>
2025-05-08 11:50:26 +0200

    cd into the dotfiles folder for stow to work

--- install/includes/stow.sh
+++ install/includes/stow.sh
@@ -1,10 +1,18 @@
 echo ":: Stowing dotfiles"
 
+# Get the current working directory
+SCRIPT=$(realpath "$0")
+temp_path=$(dirname "$SCRIPT")
+
+cd $DIR_DOTFILES
+
 # Stow the dotfiles
 echo "stowing $DIR_DOTFILES/.config -> $HOME/.config"
-stow -t $HOME/.config $DIR_DOTFILES/.config
+stow -t $HOME/.config .config
 # Stow the homefolde files
 echo "stowing $DIR_DOTFILES/home -> $HOME"
-stow -t $HOME $DIR_DOTFILES/home
+stow -t $HOME home
+
+cd $temp_path
 
 echo ":: Dotfiles stowed"
