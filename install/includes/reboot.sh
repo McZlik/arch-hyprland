4b882190fe8fea74148c6013161c71bdfb89d68d
McZlik <github@mczlik.com>
2025-05-08 11:52:29 +0200

    sudo reboot

--- install/includes/reboot.sh
+++ install/includes/reboot.sh
@@ -5,7 +5,7 @@ echo "A reboot of your system is recommended."
 echo
 if gum confirm "Do you want to reboot your system now?" ;then
     gum spin --spinner dot --title "Rebooting now..." -- sleep 3
-    systemctl reboot
+    sudo reboot
 elif [ $? -eq 130 ]; then
     exit 130
 else
