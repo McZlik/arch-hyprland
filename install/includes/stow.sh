ddad8e4ec4477ea3032e897d812f80ab54f16b93
McZlik <github@mczlik.com>
2025-05-08 11:22:18 +0200

    Adding more debug logging

--- install/includes/stow.sh
+++ install/includes/stow.sh
@@ -1,6 +1,8 @@
+echo ":: Stowing dotfiles"
+
 # Stow the dotfiles
 stow -t $HOME/.config $DIR_DOTFILES/.config
 # Stow the homefolde files
 stow -t $HOME $DIR_DOTFILES/home
 
-echo ":: Dotfiles installed"
+echo ":: Dotfiles stowed"
